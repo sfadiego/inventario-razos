6820671a29e4abec5cce52b7f680149713047c13
HugoVargas4 <albertovargas.hv@gmail.com>
2025-03-07 11:34:11 -0600

    migrations

---
+++ database/migrations/2025_03_07_170743_create_proveedores_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if (!Schema::hasTable('proveedores')) {
+            Schema::create('proveedores', function (Blueprint $table) {
+                $table->id();
+                $table->string('nombre');
+                $table->string('empresa');
+                $table->text('observaciones')->nullable();
+                $table->timestamps();
+            });
+        } 
+    }
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('proveedores');
+    }
+};
---
+++ database/migrations/2025_03_07_171337_create_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+{
+    if (!Schema::hasTable('productos')) {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->foreignId('proveedor_id')->constrained('proveedores')->onDelete('cascade');
+            $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade');
+            $table->string('codigo')->unique();
+            $table->decimal('precio_compra', 10, 2);
+            $table->decimal('precio_venta', 10, 2);
+            $table->integer('stock');
+            $table->integer('cantidad_minima');
+            $table->timestamps();
+            $table->text('compatibilidad')->nullable();
+            $table->foreignId('ubicacion_id')->constrained('ubicaciones')->onDelete('cascade');
+            $table->boolean('activo');
+        });
+    }
+}
+
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
