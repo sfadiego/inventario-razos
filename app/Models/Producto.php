60a12fd1c89857a877f3b2b8bca5b9ea5bac63a9
HugoVargas4 <albertovargas.hv@gmail.com>
2025-03-07 21:49:16 -0600

    modelos

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    protected $fillable = ['nombre','proveedor_id','categoria_id', 'codigo', 'precio_compra', 'precio_venta','stock','cantidad_minima','compatibilidad','ubicacion_id','activo'];
 }
--- app/Models/Proveedor.php
+++ app/Models/Proveedor.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Proveedor extends Model
 {
-    //
+    protected $fillable = ['nombre', 'empresa', 'observaciones'];
 }
