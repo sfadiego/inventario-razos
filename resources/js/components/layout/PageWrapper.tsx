defd8369b89753d73545eeca50dba024a1fa1d0b
dsilva <sfadiego@gmail.com>
2025-05-22 10:38:01 -0600

    ajustando template blank

--- resources/js/components/layout/PageWrapper.tsx
+++ resources/js/components/layout/PageWrapper.tsx
@@ -13,13 +13,16 @@ export const PageWrapper = ({ pageTitle, children, className = '', blankWrapper 
     return (
         <div>
             <PageMeta title={title} description={title} />
-            <PageBreadcrumb pageTitle={pageTitle} />
+
             {blankWrapper ? (
                 <div className={`${className}`}>{children}</div>
             ) : (
-                <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 xl:px-10 xl:py-12 dark:border-gray-800 dark:bg-white/[0.03]">
-                    <div className="mx-auto w-full max-w-[630px] text-center">{children}</div>
-                </div>
+                <>
+                    <PageBreadcrumb pageTitle={pageTitle} />
+                    <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 xl:px-10 xl:py-12 dark:border-gray-800 dark:bg-white/[0.03]">
+                        <div className="mx-auto w-full max-w-[630px] text-center">{children}</div>
+                    </div>
+                </>
             )}
         </div>
     );
