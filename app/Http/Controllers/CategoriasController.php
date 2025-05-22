a372ad4d498f29c0b9f60ef0dc1ca3d48c7e5296
dsilva <sfadiego@gmail.com>
2025-05-22 11:38:21 -0600

    refactor, ajuste de rutas y componentes sidebar

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\Categorias\CategoriasStoreRequest;
 use App\Http\Requests\Categorias\CategoriasUpdateRequest;
 use App\Models\Categoria;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class CategoriasController extends Controller
