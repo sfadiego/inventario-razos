078aaa4954e45a509720dad7ecd8e8f162abc872
HugoVargas4 <albertovargas.hv@gmail.com>
2025-05-23 23:53:15 -0600

    Implementa factory producto_proveedor

--- app/Models/ProductoProveedor.php
+++ app/Models/ProductoProveedor.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductoProveedor extends Model
 {
+    use HasFactory;
+
     protected $table = 'producto_proveedor';
+
+    public $timestamps = false;
 
     protected $fillable = ['proveedor_id', 'producto_id'];
 }
