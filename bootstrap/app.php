05eba109ed383cff53f4d7fd7566188bd3e3a584
dsilva <sfadiego@gmail.com>
2025-05-23 20:05:50 -0600

    datatable

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
