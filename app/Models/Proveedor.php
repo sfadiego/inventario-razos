3fc8d8392293582cde7c23e7a6d9302d83810bfb
HugoVargas4 <albertovargas.hv@gmail.com>
2025-05-24 00:34:38 -0600

    Agrega relaciones en modelos: proveedor, producto_proveedor y producto

--- app/Models/Proveedor.php
+++ app/Models/Proveedor.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Proveedor extends Model
 {
@@ -12,4 +13,14 @@ class Proveedor extends Model
     protected $table = 'proveedores';
 
     protected $fillable = ['nombre', 'empresa', 'observaciones'];
+
+    public function productos(): HasMany
+    {
+        return $this->hasMany(Producto::class);
+    }
+
+    public function productoProveedores(): HasMany
+    {
+        return $this->hasMany(ProductoProveedor::class);
+    }
 }
