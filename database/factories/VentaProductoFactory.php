54579dbe79617818226772a13dc0bb6ee143cf9e
HugoVargas4 <albertovargas.hv@gmail.com>
2025-05-28 15:21:48 -0600

    correciones y comentarios: agregando modelos y factories

---
+++ database/factories/VentaProductoFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Producto;
+use App\Models\Venta;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VentaProductoFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'cantidad' => $this->faker->randomNumber(2),
+            'precio' => $this->faker->randomFloat(2, 10, 100),
+            'producto_id' => Producto::inRandomOrder()->value('id'),
+            'venta_id' => Venta::inRandomOrder()->value('id'),
+        ];
+    }
+}
