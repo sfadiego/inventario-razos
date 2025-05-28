0c931d980b9c58137b524695145d7bd9478158e0
HugoVargas4 <albertovargas.hv@gmail.com>
2025-05-27 22:04:05 -0600

    Migraciones: crea tabla venta y actualiza venta_producto

---
+++ database/migrations/2025_03_07_171925_create_venta_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('venta', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('venta_total', 10, 2);
+            $table->string('nombre_venta');
+            $table->foreignId('cliente_id')
+                ->constrained('clientes')
+                ->onDelete('cascade');
+            $table->string('tipo_compra')
+                ->default('contado');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('venta');
+    }
+};
