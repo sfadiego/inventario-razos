25e38d0cd9eddec7790fd377bf0a52f17dd38025
dsilva <sfadiego@gmail.com>
2025-06-05 16:15:33 -0600

    ventas

--- resources/js/pages/Venta/VentasPage.tsx
+++ resources/js/pages/Venta/VentasPage.tsx
@@ -1,13 +1,25 @@
 import { PageWrapper } from '@/components/layout/PageWrapper';
+import Button from '@/components/ui/button/Button';
+import { Banknote } from 'lucide-react';
 
 export default function VentasPage() {
+    const nuevaVenta = () => {};
     return (
         <PageWrapper pageTitle="Ventas">
-            <h3 className="text-theme-xl mb-4 font-semibold text-gray-800 sm:text-2xl dark:text-white/90">Card Title Here</h3>
-            <p className="text-sm text-gray-500 sm:text-base dark:text-gray-400">
-                Start putting content on grids or panels, you can also use different combinations of grids.Please check out the dashboard and other
-                pages
-            </p>
+            <>
+                <div className="grid grid-cols-12 gap-2 pb-5">
+                    <div className="col-span-10"></div>
+                    <div className="col-span-2">
+                        <Button onClick={nuevaVenta} className="w-full">
+                            <Banknote /> Nueva venta
+                        </Button>
+                    </div>
+                </div>
+                <div className="grid grid-cols-12 gap-2 pb-5">
+                    <div className="col-span-12"></div>
+                    <div className="col-span-12"></div>
+                </div>
+            </>
         </PageWrapper>
     );
 }
