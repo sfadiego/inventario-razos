1a6d72a34ae4dcfa1803fc33e4f5a047d79e2fd2
dsilva <sfadiego@gmail.com>
2025-06-06 09:52:42 -0600

    quitando componentes innecesarios, ajuste de componente check

--- resources/js/components/form/input/Checkbox.tsx
+++ resources/js/components/form/input/Checkbox.tsx
@@ -1,82 +1,49 @@
-import type React from "react";
+import { FormikProps } from 'formik';
+import { Check } from 'lucide-react';
 
-interface CheckboxProps {
-  label?: string;
-  checked: boolean;
-  className?: string;
-  id?: string;
-  onChange: (checked: boolean) => void;
-  disabled?: boolean;
+interface CheckboxProps<T> {
+    label?: string;
+    checked: boolean;
+    className?: string;
+    id?: string;
+    onChange: (checked: boolean) => void;
+    disabled?: boolean;
+    formik: FormikProps<T>;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({
-  label,
-  checked,
-  id,
-  onChange,
-  className = "",
-  disabled = false,
-}) => {
-  return (
-    <label
-      className={`flex items-center space-x-3 group cursor-pointer ${
-        disabled ? "cursor-not-allowed opacity-60" : ""
-      }`}
-    >
-      <div className="relative w-5 h-5">
-        <input
-          id={id}
-          type="checkbox"
-          className={`w-5 h-5 appearance-none cursor-pointer dark:border-gray-700 border border-gray-300 checked:border-transparent rounded-md checked:bg-brand-500 disabled:opacity-60 
-          ${className}`}
-          checked={checked}
-          onChange={(e) => onChange(e.target.checked)}
-          disabled={disabled}
-        />
-        {checked && (
-          <svg
-            className="absolute transform -translate-x-1/2 -translate-y-1/2 pointer-events-none top-1/2 left-1/2"
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="0 0 14 14"
-            fill="none"
-          >
-            <path
-              d="M11.6666 3.5L5.24992 9.91667L2.33325 7"
-              stroke="white"
-              strokeWidth="1.94437"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        )}
-        {disabled && (
-          <svg
-            className="absolute transform -translate-x-1/2 -translate-y-1/2 pointer-events-none top-1/2 left-1/2"
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="0 0 14 14"
-            fill="none"
-          >
-            <path
-              d="M11.6666 3.5L5.24992 9.91667L2.33325 7"
-              stroke="#E4E7EC"
-              strokeWidth="2.33333"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        )}
-      </div>
-      {label && (
-        <span className="text-sm font-medium text-gray-800 dark:text-gray-200">
-          {label}
-        </span>
-      )}
-    </label>
-  );
+const Checkbox = <T extends object>({ label, checked, id, onChange, className = '', disabled = false }: CheckboxProps<T>) => {
+    return (
+        <label className={`group flex cursor-pointer items-center space-x-3 ${disabled ? 'cursor-not-allowed opacity-60' : ''}`}>
+            <div className="relative h-5 w-5">
+                <input
+                    id={id}
+                    name={id}
+                    type="checkbox"
+                    className={`checked:bg-brand-500 h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-300 checked:border-transparent disabled:opacity-60 dark:border-gray-700 ${className}`}
+                    checked={checked}
+                    onChange={(e) => onChange(e.target.checked)}
+                    disabled={disabled}
+                />
+                {checked && (
+                    <Check
+                        width="14"
+                        height="14"
+                        fill="none"
+                        className="stroke-white pointer-events-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+                    />
+                )}
+                {disabled && (
+                    <Check
+                        width="14"
+                        height="14"
+                        fill="none"
+                        className="pointer-events-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+                    />
+                )}
+            </div>
+            {label && <span className="text-sm font-medium text-gray-800 dark:text-gray-200">{label}</span>}
+        </label>
+    );
 };
 
 export default Checkbox;
