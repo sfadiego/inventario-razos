3a423add109983820066a13f0a6b2bd98dd8e2a9
dsilva <sfadiego@gmail.com>
2025-06-10 11:15:18 -0600

    todo ok

--- routes/modules/proveedores.php
+++ routes/modules/proveedores.php
@@ -7,7 +7,7 @@ Route::controller(ProveedoresController::class)
     ->group(function () {
         Route::get('', 'index');
         Route::post('', 'store');
-        Route::prefix('{id}')->group(function () {
+        Route::prefix('{proveedor}')->group(function () {
             Route::get('', 'show');
             Route::put('', 'update');
             Route::delete('', 'destroy');
