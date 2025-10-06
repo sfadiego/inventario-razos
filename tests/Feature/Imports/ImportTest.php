e267cbb6d0c97ac3c41eb86adf8324c2b778bbba
dsilva <sfadiego@gmail.com>
2025-10-06 14:19:32 -0600

    implementando test de import

--- tests/Feature/Imports/ImportTest.php
+++ tests/Feature/Imports/ImportTest.php
@@ -2,19 +2,38 @@
 
 namespace Tests\Feature\Imports;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Producto;
 use Tests\TestCase;
-use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
 
 class ImportTest extends TestCase
 {
-    public function test_import_productos_correctamente(): void
+    public function test_import_products_successfully(): void
     {
+        $this->loginAdmin();
+        $totalProductosAntes = Producto::count();
+        $filePath = base_path('tests/Feature/Imports/productos.xlsx');
+        $file = new UploadedFile(
+            $filePath,
+            'productos.xlsx',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            null,
+            true
+        );
         $response = $this->post('/api/imports', [
-            'file' => new File(public_path('tests/Feature/Imports/productos.csv')),
+            'file' => $file,
         ]);
 
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+            'data' => [
+                'inserted',
+            ],
+        ]);
+
+        $totalInsertados = count($response->json('data.inserted'));
+        $this->assertEquals($totalProductosAntes + $totalInsertados, Producto::count());
     }
 }
