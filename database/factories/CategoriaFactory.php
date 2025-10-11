64b50d16a533e2dd9950963847113e35205570d7
HugoVargas4 <albertovargas.hv@gmail.com>
2025-10-11 13:25:38 -0600

    agrega test categoria

---
+++ database/factories/CategoriaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AppModelsCliente>
+ */
+class CategoriaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->name,
+            'activa' => $this->faker->boolean,
+        ];
+    }
+}
