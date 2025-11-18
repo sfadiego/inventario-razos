55c1e7fa34aae26b02b40793a3dc3e2870d2dd87
Diego Silva <sfadiego@gmail.com>
2025-11-18 14:56:41 -0600

    test

--- database/factories/VentaFactory.php
+++ database/factories/VentaFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Enums\StatusVentaEnum;
 use App\Models\Cliente;
-use App\Models\Venta;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VentaFactory extends Factory
