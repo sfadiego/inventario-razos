ee2bf251d8fb7b0317a7d1f14a2f72279072601e
Diego Silva <sfadiego@gmail.com>
2026-01-09 14:27:34 -0600

    format

--- resources/js/pages/Dashboard/Home.tsx
+++ resources/js/pages/Dashboard/Home.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <div className="col-span-12 xl:col-span-5">
             <MonthlySalesChart />
           </div>
-          <div className="col-span-12 xl:col-span-7 mb-6">
+          <div className="col-span-12 mb-6 xl:col-span-7">
             <RecentOrders />
           </div>
         </div>
