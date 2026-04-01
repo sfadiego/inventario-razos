f70b9aae3da265db9cb911f379fd62ddf61b4048
Diego Silva <sfadiego@gmail.com>
2026-04-01 11:42:22 -0600

    formato ticket

--- app/Printer/Formatters/VentaFormatter.php
+++ app/Printer/Formatters/VentaFormatter.php
@@ -18,7 +18,6 @@ class VentaFormatter implements TicketFormatterInterface
         $venta_total = $payload['venta_total'];
         $venta_nombre = $payload['nombre_venta'];
         $venta_folio = $payload['folio'];
-        $venta_tipo_compra = $payload['tipo_compra'];
         $venta_created_at = $payload['created_at'];
         $venta_items = $payload['ventaProductos'];
 
@@ -26,26 +25,27 @@ class VentaFormatter implements TicketFormatterInterface
         $connector->text('Vendido a:');
         $connector->text($venta_nombre !== '' ? $venta_nombre : ' sin nombre ');
         $connector->feed(1);
-        $connector->text('Folio: '.$venta_folio);
+        $connector->text('Folio: ' . $venta_folio);
         $connector->feed(1);
-        $connector->text('Tipo de compra: '.$venta_tipo_compra);
-        $connector->feed(1);
-        $connector->text('Fecha: '.$venta_created_at);
+        $connector->text('Fecha: ' . $venta_created_at);
         $connector->feed(2);
 
         $connector->text('Precio | Cantidad | Producto | Codigo');
         $connector->feed(1);
         collect($venta_items)
             ->each(function ($item) use ($connector) {
-                $connector->text('$'.$item['precio'].' x '.$item['cantidad'].'pzas - '.$item['producto_nombre']);
-                $connector->text(' - '.$item['codigo']."\n");
+                $connector->text('$' . $item['precio'] . ' x ' . $item['cantidad'] . 'pzas - ' . $item['producto_nombre']);
+                $connector->text(' - ' . $item['codigo'] . "\n");
                 $connector->feed(1);
             });
 
-        $connector->text('Total: $'.$venta_total);
+        $connector->text('Total: $' . $venta_total);
         $connector->feed(2);
         $connector->setJustification(Printer::JUSTIFY_CENTER);
         $connector->text('Gracias por su compra');
+        $connector->text('Devolución o aclaración únicamente con ticket');
+        $contacto = env('WHATSSAPP_CONTACTO', '');
+        $connector->text('WhatsApp: ' . $contacto);
         $connector->feed(3);
     }
 }
